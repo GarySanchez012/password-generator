5832fd9991768d0c8222c36ab3f945c29263ab45
arroyhondo <garysanchez012@gmail.com>
2021-06-12 14:54:43 -0400

    added proffesors block code for JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,48 @@
 // Assignment code here
 
+var generatePassword = function () {
+  var finalPassword = "aw" //finalPassword.length
+  var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  var uppercase = "ABCDEFGHIJKLONOPQRSTUVWXYZ"
+  var specialCharcters = "%$#()"
+  var number = "0123456789"
+  var passwordsize = prompt("How many characters would you like your password to contain?")
+  console.log(passwordsize)
+  var isLowerCase = confirm("Does your password include lowercase?")
+  
+  var isUpperCase = confirm("Does your password include Uppercase?")
+  
+  var isSpecialCharacter= confirm("Does your password include  special?")
+  
+  var isNumber = confirm("Does your password include number?")
+  console.log(isLowerCase)
+  // ending 
+  
+  if (isLowerCase) {
+    var randomPickLowerCase = Math.floor(Math.random() * lowercase.length)
+    var pickOneLowerCaseLetter = lowercase.charAt(randomPickLowerCase)
+    finalPassword =finalPassword+ pickOneLowerCaseLetter 
+  }
+  if(isUpperCase){
+    var randomPickUpperCase = Math.floor(Math.random() * uppercase.length)
+    var pickOneUpperCaseLetter = uppercase.charAt(randomPickUpperCase)
+    finalPassword =finalPassword+ pickOneUpperCaseLetter
+  }
+  if(isSpecialCharacter){
+    var randomPickSpecial = Math.floor(Math.random() * specialCharcters.length)
+    var pickOneSpecialCharacterLetter = specialCharcters.charAt(randomPickSpecial)
+    finalPassword =finalPassword+ pickOneSpecialCharacterLetter
+  }
+  if(isNumber){
+    var randomPickNumber = Math.floor(Math.random() * number.length)
+    var pickOneNumber = number.charAt(randomPickNumber)
+    finalPassword =finalPassword+  pickOneNumber
+  }
+  console.log(pickOneLowerCaseLetter, pickOneUpperCaseLetter, pickOneSpecialCharacterLetter, pickOneNumber)
+  return finalPassword
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +58,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
