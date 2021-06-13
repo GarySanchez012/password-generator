f7cc60834a0b00288dc9a08fd805ac24bfb534e0
arroyhondo <garysanchez012@gmail.com>
2021-06-13 18:53:09 -0400

    added a way to get out of the while loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,42 +7,50 @@ var generatePassword = function () {
   var upperCase = "ABCDEFGHIJKLONOPQRSTUVWXYZ"
   var specialCharcters = "!@#$%^&*()[]{}\|;:',<.>/?"
   var number = "0123456789"
-  var passwordSize = parseInt(prompt("How many characters would you like your password to contain?"));
-  while (Number.isNaN(passwordSize) || passwordSize < 8 || passwordSize >128)
-  console.log(passwordSize)
-
+  var passwordSizePrompt = prompt("How many characters would you like your password to contain? (8-128)");
+  var passwordSize = parseInt(passwordSizePrompt)
+  if (passwordSizePrompt === null){
+    return finalPassword;
+  }
   
-  var isLowerCase = confirm("Does your password include lowerCase?")
+  while (Number.isNaN(passwordSize) || passwordSize < 8 || passwordSize >128){
+    if (passwordSizePrompt === null){
+      return finalPassword;
+    }
+    passwordSizePrompt = prompt("How many characters would you like your password to contain? (8-128)");
+    passwordSize = parseInt(passwordSizePrompt)
+    console.log(passwordSize)
+  }
   
-  var isUpperCase = confirm("Does your password include UpperCase?")
+  var isLowerCase = confirm("Does your password include lower case?")
   
-  var isSpecialCharacter= confirm("Does your password include special?")
+  var isUpperCase = confirm("Does your password include upper case?")
+  
+  var isSpecialCharacter= confirm("Does your password include special characters?")
   
   var isNumber = confirm("Does your password include number?")
-  console.log(isLowerCase)
   // ending 
 
   
   if (isLowerCase) {
     passwordOptions += lowerCase
-    finalPassword += lowerCase.charAt(Math.floor(Math.random())*lowerCase.length)
+    finalPassword += lowerCase.charAt(Math.floor(Math.random()*lowerCase.length))
   }
   if(isUpperCase){
     passwordOptions += upperCase
-    finalPassword += upperCase.charAt(Math.floor(Math.random())*upperCase.length)
+    finalPassword += upperCase.charAt(Math.floor(Math.random()*upperCase.length))
   }
   if(isSpecialCharacter){
     passwordOptions += specialCharcters
-    finalPassword += specialCharcters.charAt(Math.floor(Math.random())*specialCharcters.length)
+    finalPassword += specialCharcters.charAt(Math.floor(Math.random()*specialCharcters.length))
   }
   if(isNumber){
     passwordOptions += number
-    finalPassword += number.charAt(Math.floor(Math.random())*number.length)
+    finalPassword += number.charAt(Math.floor(Math.random()*number.length))
   }
   for(var i = finalPassword.length; i<passwordSize; i++){
     finalPassword += passwordOptions.charAt(Math.floor(Math.random()*passwordOptions.length));
   }
-  console.log(finalPassword);
   return finalPassword
 }
 
